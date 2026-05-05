258c8e0cd90562179cfd70fe6bb3254134b57703
wayne <wayne.camara@sitelbra.com.br>
2026-05-05 13:55:03 -0300

    feat: Creation of the abstract method, person, receiving cpf, age, and name as parameters. Thus fulfilling what is requested by card 1.1

--- src/main/java/com/grupo8/projeto_poo_grupo_28/ProjetoPooGrupo28Application.java
+++ src/main/java/com/grupo8/projeto_poo_grupo_28/ProjetoPooGrupo28Application.java
@@ -3,6 +3,40 @@ package com.grupo8.projeto_poo_grupo_28;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Abstração é o processo de ocultar detalhes complexos de implementação e
+// mostrar apenas os recursos essenciais de um objeto.
+public abstract class Pessoa {
+	private String cpf;
+	private String nome;
+	private int idade;
+
+	// getter
+	public String getCpf() {
+		return cpf;
+	}
+
+	// setter
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+}
+
 @SpringBootApplication
 public class ProjetoPooGrupo28Application {
 
