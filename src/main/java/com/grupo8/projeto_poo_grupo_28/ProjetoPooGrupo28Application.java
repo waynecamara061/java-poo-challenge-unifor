c1ba217ff7d70b2dccd12529c8668d4fb1f74ce7
wayne <wayne.camara@sitelbra.com.br>
2026-05-04 21:56:47 -0300

    first commit

---
+++ src/main/java/com/grupo8/projeto_poo_grupo_28/ProjetoPooGrupo28Application.java
@@ -0,0 +1,13 @@
+package com.grupo8.projeto_poo_grupo_28;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoPooGrupo28Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoPooGrupo28Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/grupo8/projeto_poo_grupo_28/ProjetoPooGrupo28ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo8.projeto_poo_grupo_28;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoPooGrupo28ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
