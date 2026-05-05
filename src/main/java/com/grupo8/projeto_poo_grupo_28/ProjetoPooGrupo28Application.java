e51224b75459647668d4fcf9275e640d55c503cf
wayne <wayne.camara@sitelbra.com.br>
2026-05-05 14:04:42 -0300

    feat: Creation of the abstract method, dicipline, code and semester parameters. Thus fulfilling what is requested by card 1.2

--- src/main/java/com/grupo8/projeto_poo_grupo_28/ProjetoPooGrupo28Application.java
+++ src/main/java/com/grupo8/projeto_poo_grupo_28/ProjetoPooGrupo28Application.java
@@ -3,6 +3,37 @@ package com.grupo8.projeto_poo_grupo_28;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+public class Diciplina {
+	private String codigo;
+	private String nome;
+	private String semestre;
+
+	// Getters e Setters
+	public String getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSemestre() {
+		return semestre;
+	}
+
+	public void setSemestre(String semestre) {
+		this.semestre = semestre;
+	}
+}
+
 @SpringBootApplication
 public class ProjetoPooGrupo28Application {
 
