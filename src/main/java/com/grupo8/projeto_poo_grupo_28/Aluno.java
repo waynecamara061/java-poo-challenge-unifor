cbc5ac3b487da404bc9d3c0c5d8b6f223499fea9
wayne <wayne.camara@sitelbra.com.br>
2026-05-05 17:54:54 -0300

    feat: implementar estrutura de herança de pessoa com as classes Visitante, Aluno e Professor, além da organização dos arquivos dentro de /src/com/group28/projeto_poo_group_28 ...

---
+++ src/main/java/com/grupo8/projeto_poo_grupo_28/Aluno.java
@@ -0,0 +1,9 @@
+package com.grupo8.projeto_poo_grupo_28;
+
+public class Aluno extends Pessoa {
+	// TODO; passar atributo Aluno (Adicionar atributo matrícula e o método
+	// pagarMensalidade().)
+
+	// TODO: Especialização de Aluno: Criar as classes Bolsista e Regular, aplicando
+	// sobreposição (override) no método pagarMensalidade() em ambas.
+}
