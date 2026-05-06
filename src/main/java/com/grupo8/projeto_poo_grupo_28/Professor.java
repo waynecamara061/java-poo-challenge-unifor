20648d0d9431c9af2f30e1216f535543a9bbb89a
wayne <wayne.camara@sitelbra.com.br>
2026-05-06 12:47:05 -0300

    feat:add centro attribute and darAula method to Professor class

--- src/main/java/com/grupo8/projeto_poo_grupo_28/Professor.java
+++ src/main/java/com/grupo8/projeto_poo_grupo_28/Professor.java
@@ -1,6 +1,17 @@
 package com.grupo8.projeto_poo_grupo_28;
 
 public class Professor extends Pessoa {
-	// TODO; passar atributo professor (Adicionar atributo centro e o método
-	// darAula().)
+	private String centro;
+
+	public void darAula() {
+		System.out.println("O professor" + this.getNome() + "esta dando aula." + this.centro);
+	}
+
+	public String getCentro() {
+		return centro;
+	}
+
+	public void setCentro(String centro) {
+		this.centro = centro;
+	}
 }
